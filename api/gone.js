1e210ba6a6cfd407e6d8bc73948b1d0fd100a945
Lastorder-DC <18280396+Lastorder-DC@users.noreply.github.com>
2022-11-09 11:09:28 +0000

    edit code

--- api/gone.js
+++ api/gone.js
@@ -1,6 +1,8 @@
-const fs = require("fs");
+import { readFileSync } from 'fs';
+import path from 'path';
 
 export default function gone(req, res) {
+  const file = path.join(process.cwd(), 'templates', '410.html');
   res.setHeader('content-type', 'text/html');
-  res.status(410).send(fs.readFileSync('410.html', {encoding: 'utf-8', flag: 'r'}));
+  res.status(410).send(readFileSync(file, {encoding: 'utf-8', flag: 'r'}));
 }
