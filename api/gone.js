8f04ea27d82c809d729a1c47baebd404c87b48e9
Lastorder-DC <18280396+Lastorder-DC@users.noreply.github.com>
2022-11-09 11:03:37 +0000

    change route

--- api/gone.js
+++ api/gone.js
@@ -1,4 +1,4 @@
-export default function handler(_, res) {
+export default function gone(req, res) {
   res.setHeader('content-type', 'text/html');
   res.status(410).send(fs.readFileSync('410.html', {encoding: 'utf-8', flag: 'r'}));
 }
